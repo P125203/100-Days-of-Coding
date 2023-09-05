6853cc4b2e941d1a7e9109a43db95498d4bd24b0
P125203 <101242922+P125203@users.noreply.github.com>
2023-09-05 19:39:43 +0530

    Add files via upload

---
+++ Day_001.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Day_001 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();	//Taking array size from user. 
+		int arr[]=new int[n];
+		
+		//Taking array input from user.
+		for(int i = 0 ; i < n ; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		//Sorting the array in ascending order using bubble sort.
+		for(int i = 0 ; i < n ; i++) {
+			for(int j = i+1 ; j < n ; j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+//		System.out.println(Arrays.toString(arr));	//Arrays method to print array.
+		
+		//Loop for printing sorted array.
+		for(int i = 0 ; i < n ; i++) {
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+
+}
