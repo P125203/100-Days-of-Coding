0d776b87d03ab6d2657d733bb716f3c2cb4d218f
P125203 <101242922+P125203@users.noreply.github.com>
2023-09-12 23:52:49 +0530

    Add files via upload

---
+++ Day_007.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Day_007 {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        
+        return j;
+    }
+
+    public static void main(String[] args) {
+        Day_007 solution = new Day_007();
+        int[] nums = {1, 1, 2, 2, 2, 3, 4, 4, 5};
+        int length = solution.removeDuplicates(nums);
+        
+        System.out.println("Modified Array:");
+        for (int i = 0; i < length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
