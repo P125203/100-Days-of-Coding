f3db2490d62269af67297c2b883430af458fe4ba
P125203 <101242922+P125203@users.noreply.github.com>
2023-09-15 22:50:44 +0530

    Add files via upload

---
+++ Day_010.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Day_010 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String substr = "";
+		int n = 0;
+		
+		for(int i = 0 ; i < str.length() ; i++) {
+			if(Character.isAlphabetic(str.charAt(i)) == true) {
+				substr = substr + str.charAt(i);
+			}
+//			if(Character.isDigit(str.charAt(i)) == true) {
+//				n = n*10 + Character.getNumericValue(str.charAt(i));
+//			}
+		}
+		System.out.print(substr);
+
+	}
+
+}
