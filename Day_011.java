bd0adb48299c4995155bbc6690d70e7ffc35f563
P125203 <101242922+P125203@users.noreply.github.com>
2023-09-16 22:40:48 +0530

    Add files via upload

---
+++ Day_011.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Day_011 {
+	
+	public static int factorial(int x) {
+		if(x >= 1)return x * factorial(x-1);
+		return 1;
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		System.out.println(factorial(x));
+
+	}
+}
