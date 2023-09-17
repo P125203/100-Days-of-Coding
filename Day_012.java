b86e8d723f47bb163c7fd998e38a446a9b36ef0f
P125203 <101242922+P125203@users.noreply.github.com>
2023-09-17 17:47:25 +0530

    Add files via upload

---
+++ Day_012.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Day_012 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		
+		int count = 0;
+		for(int i = 0 ; i < s.length() ; i++) {
+			if(i < s.length())
+				count++;
+		}
+		System.out.print(count);
+	}
+}
